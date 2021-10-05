61f894a1c38133c05b12edca45326b1d611e6167
Andrei Veres <imfzmn@yahoo.com>
2021-10-05 08:17:14 +0300

    finish lab2

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 def is_palindrome(n):
     x=n
     a=0
-    print(x)
     while x!=0:
         a=a*10+x%10
         x=x//10
@@ -39,10 +38,25 @@ def test_is_superprime():
     assert is_superprime(222) is False
     assert is_superprime(293) is True
 
+
+def get_largest_prime_below(n):
+    for i in range(n-1,0,-1):
+        if prim(i)==True:
+            return i
+    b="Nu exista"
+    return b
+
+def test_get_largest_prime_below():
+    assert get_largest_prime_below(5) == 3
+    assert get_largest_prime_below(10) == 7
+    assert get_largest_prime_below(2) == "Nu exista"
+
+
 def main():
     print("Alegeti optiunea:\n"
           "1.Testati daca un nr este palindrom\n"
-          "2.Testati daca un nr este superprim\n")
+          "2.Testati daca un nr este superprim\n"
+          "3.Gaseste ultimul nr prim mai mic decat numarul dat\n")
     n=int(input())
     if n==1:
         x=int(input("Ce numar vreti sa testati?\n"))
@@ -50,10 +64,12 @@ def main():
     elif n==2:
         x = int(input("Ce numar vreti sa testati?\n"))
         print(is_superprime(x))
+    elif n==3:
+        x= int(input("Ce numer vreti sa tastati?\n"))
+        print(get_largest_prime_below(x))
     else:
         print("Ati ales un numar incorect , tastati din nou\n")
         main()
-
 main()
 
 
